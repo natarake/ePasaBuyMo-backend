e3c3f822b40f1f997315b0071514ff137a0a2a70
natarake <30865043+natarake@users.noreply.github.com>
2023-01-13 18:01:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,18 @@ mongoose
   });
 
 app.use(cors({origin:true,credentials: true}));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 app.options('*', cors());
 app.options('/*', (_, res) => {
     res.sendStatus(200);
